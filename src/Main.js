7ded1fc1e67a53205cbaa11044b713a1b40400e6
Guangyao Li <ligyj@chanjet.com>
2019-01-23 13:45:46 +0800

    列表组件，添加上 key

--- src/Main.js
+++ src/Main.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class Main extends Component {
-    
-    constructor(props) {
-        super(props)
-        this.state = {
-            books: [
-                'you dont know js',
-                '那么慢，那么美'
-            ]
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: ["you dont know js", "那么慢，那么美"],
     }
+  }
 
-    addBook = () => {
-        this.setState(preState => {
-            preState.books.push(['今日简史'])
-            return {
-                books: preState.books
-            }
-        })
-    }
+  addBook = () => {
+    this.setState(preState => {
+      preState.books.push(["今日简史"])
+      return {
+        books: preState.books,
+      }
+    })
+  }
 
-    render() {
-        const books = this.state.books
-        return (
-            <div>
-                <button onClick={this.addBook}>添加</button>
-                <ul>
-                    {
-                        books.map(book => <li>{book}</li>)
-                    }
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    const books = this.state.books
+    return (
+      <div>
+        <button onClick={this.addBook}>添加</button>
+        <ul>
+          {books.map((book, index) => (
+            <li key={index}>{book}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default Main
